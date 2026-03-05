d1392a265e505ac446dde621f6accb479407a573
raguld812-sys <raguld812@gmail.com>
2026-03-05 21:14:32 +0530

    initialize

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,6 @@
 public class OOPSBannerApp {
-    Run | Debug
-    public static void main(String[] args) {
+    
+public static void main(String[] args) {
         System.out.println(x:"OOPS");
     }
 }
