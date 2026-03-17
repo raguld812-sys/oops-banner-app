fb2fdeb460a373b77ad697c4570127942ae21616
raguld812-sys <raguld812@gmail.com>
2026-03-17 21:49:55 +0530

    initialize feature/uc5

---
+++ stringjoin(lines).java
@@ -0,0 +1,15 @@
+public class stringjoin(lines) {
+
+    public static void main(String[] args) {
+
+       line[0] = System.out.println(string.join("   ***   " + "   ***   " + "*********" + "*********"));
+       line[1] = System.out.println(string.join("  ** **  " + "  ** **  " + "*********" + "*********"));
+       line[2] = System.out.println(string.join(" **   ** " + " **   ** " + "***   ** " + "**      "));
+       line[3] = System.out.println(string.join(" **   ** " + " **   ** " + "******** " + "********"));
+       line[4] =  System.out.println(string.join(" **   ** " + " **   ** " + "******** " + "********"));
+       line[5] = System.out.println(string.join("  ** **  " + "  ** **  " + "***      " + "**      "));
+       line[6] = System.out.println(string.join("   ***   " + "   ***   " + "***      " + "********"));
+        
+    }
+
+}
