3ec07320b8f8528af0afaf2793fcfe3cac11fc5f
raguld812-sys <raguld812@gmail.com>
2026-03-29 12:39:11 +0530

    initialize oopspattern

---
+++ stringjoin.java
@@ -0,0 +1,15 @@
+public class stringjoin {
+
+    public static void main(String[] args) {
+
+        System.out.println(string.join("   ***   " + "   ***   " + "*********" + "*********"));
+        System.out.println(string.join("  ** **  " + "  ** **  " + "*********" + "*********"));
+        System.out.println(string.join(" **   ** " + " **   ** " + "***   ** " + "**      "));
+        System.out.println(string.join(" **   ** " + " **   ** " + "******** " + "********"));
+        System.out.println(string.join(" **   ** " + " **   ** " + "******** " + "********"));
+        System.out.println(string.join("  ** **  " + "  ** **  " + "***      " + "**      "));
+        System.out.println(string.join("   ***   " + "   ***   " + "***      " + "********"));
+        
+    }
+
+}
